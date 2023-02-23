98de5904007e3fa2bd85c7e70ce940cd6eb5c005
JOAO PEDRO LOPES VICENTIN <jplvicen@gmail.com>
2023-02-22 23:41:15 -0300

    .

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import headerBg from "../../assets/headerBackground.png"
 
 export const HeaderContainer = styled.main`
     min-height: 18.5rem;
     width: 100%;
     margin: 0 auto;
-    background: url('src/assets/headerBackground.png');
+    background: url(${headerBg});
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
